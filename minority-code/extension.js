56869cb59db77509acd7db07aeef6efe20cae07f
Highlaw <choiyool00@gmail.com>
2023-05-07 02:09:58 +0900

    LAST MODIFIED

--- minority-code/extension.js
+++ minority-code/extension.js
@@ -3,73 +3,85 @@ const axios = require("axios").default;
 const fs = require("fs");
 const path = require("path");
 
-const MakeCommentDisposable = require("./modules/MakeComment");
-// const AutoCompleteProvider = require("./modules/AutoCompleteProvider");
-const myModule = require("./modules/AutoCompleteProvider");
+const AutoCompleteProviderModule = require("./modules/AutoCompleteProvider");
+const MakeCommentDisposableModule = require("./modules/MakeComment");
 
-/** 기본 커맨드 */
-const disposable = vscode.commands.registerCommand(
-  "minority-code.helloWorld",
-  function () {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from Minority Code!");
-  }
-);
-
+/** 처음 폴더를 열었을 때 실행되는 함수 */
 const initExtension = async (context) => {
-  // const configPath = path.join(__dirname);
-  // let data = JSON.parse(fs.readFileSync(configPath, "utf-8"));
-
   let username = await context.globalState.get("username");
   let branches = await context.globalState.get("branches");
+  let isExist = false;
+
+  await context.globalState.update("username", undefined);
+  await context.globalState.update("branches", undefined);
+  await context.workspaceState.update("organization", undefined);
 
   const currentPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
 
   if (!username) {
     console.log("no user name");
-    await vscode.window.showInputBox().then(async (input) => {
-      vscode.window.showInformationMessage("your github id is" + input);
-      username = input;
-      await context.globalState.update("username", username);
-    });
+    await vscode.window
+      .showInputBox({ placeHolder: "Type Your Github ID" })
+      .then(async (input) => {
+        vscode.window.showInformationMessage("your github id is" + input);
+        username = input;
+        await context.globalState.update("username", username);
+      });
   }
   if (!branches) {
     console.log("no branches info");
-    vscode.window.showInputBox().then(async (input) => {
-      vscode.window.showInformationMessage("your organization is" + input);
-      const newBranch = [];
-      newBranch.push({
-        organization: input,
-        directory: currentPath,
-      });
+    await vscode.window
+      .showInputBox({ placeHolder: "Type Your Organization Name" })
+      .then(async (input) => {
+        vscode.window.showInformationMessage("your organization is" + input);
+        if (input) {
+          console.log("input is true");
+          const newBranch = [];
+          newBranch.push({
+            organization: input,
+            directory: currentPath,
+          });
 
-      await context.globalState.update("branches", newBranch);
-    });
+          await context.workspaceState.update("organization", input);
+          await context.globalState.update("branches", newBranch);
+          isExist = !isExist;
+        }
+      });
   } else {
-    let isExist = false;
     for (const elem of branches) {
       if (elem.directory === currentPath) {
         isExist = !isExist;
+        const organizationName = await context.workspaceState.get(
+          "organization"
+        );
+        if (!organizationName) {
+          await context.workspaceState.update(
+            "organization",
+            elem.organization
+          );
+        }
         break;
       }
     }
+  }
+  console.log(isExist);
 
-    if (!isExist) {
-      vscode.window.showInputBox().then(async (input) => {
+  if (!isExist) {
+    await vscode.window
+      .showInputBox({ placeHolder: "Type Your Organization Name" })
+      .then(async (input) => {
         vscode.window.showInformationMessage("your organization is" + input);
-        const branch = {
-          organization: input,
-          directory: currentPath,
-        };
-        const newBranches = [...branches, branch];
-        await context.globalState.update("branches", newBranches);
-        const result = await context.globalState.get("branches");
-        console.log(result);
+        if (input) {
+          const branch = {
+            organization: input,
+            directory: currentPath,
+          };
+          const newBranches = [...branches, branch];
+
+          await context.workspaceState.update("organization", input);
+          await context.globalState.update("branches", newBranches);
+        }
       });
-    }
-    console.log(branches);
   }
 };
 
@@ -77,19 +89,16 @@ const initExtension = async (context) => {
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "minority-code" is now active!');
 
   initExtension(context);
 
-  const AutoCompleteProvider = myModule(context);
+  // 아래의 두 개 객체는 주석 생성 모듈과 깃허브 연동 자동완성 기능 모듈입니다.
+  // 해당 두 개의 객체를 context.subscriptions에 push해야 기능이 작동합니다.
+  const MakeCommentDisposable = MakeCommentDisposableModule(context);
+  const AutoCompleteProvider = AutoCompleteProviderModule(context);
 
-  context.subscriptions.push(
-    disposable,
-    MakeCommentDisposable,
-    AutoCompleteProvider
-  );
+  context.subscriptions.push(MakeCommentDisposable, AutoCompleteProvider);
 }
 
 // This method is called when your extension is deactivated
--- minority-code/modules/AutoCompleteProvider.js
+++ minority-code/modules/AutoCompleteProvider.js
@@ -7,54 +7,6 @@ const envPath = path.join(__dirname, "../", ".env");
 const dotenv = require("dotenv").config({ path: envPath });
 const KEY = process.env.GITHUB_API_KEY;
 
-const callGithubAPI = async (organizationName) => {
-  const response = await axios.get(
-    `https://api.github.com/orgs/${organizationName}/members`,
-    {
-      headers: {
-        Accept: "application/vnd.github+json",
-        Authorization: KEY,
-      },
-    }
-  );
-  return response;
-};
-
-/** 자동완성 Provider */
-const AutoCompleteProvider = vscode.languages.registerCompletionItemProvider(
-  ["c", "cpp", "java", "javascript"],
-  {
-    async provideCompletionItems(document, position, token, context) {
-      const linePrefix = document
-        .lineAt(position)
-        .text.substring(0, position.character);
-      if (!linePrefix.endsWith(" * BY @") && !linePrefix.endsWith("/* TO @")) {
-        return undefined;
-      }
-
-      // const response = await axios.get(
-      //   `https://api.github.com/orgs/${organizationName}/members`,
-      //   {
-      //     headers: {
-      //       Accept: "application/vnd.github+json",
-      //       Authorization: KEY,
-      //     },
-      //   }
-      // );
-
-      // const completionItems = response.data.map((elem) => {
-      //   return new vscode.CompletionItem(
-      //     elem.login,
-      //     vscode.CompletionItemKind.User
-      //   );
-      // });
-
-      // return completionItems;
-    },
-  },
-  "@"
-);
-
 module.exports = function (parentContext) {
   const AutoCompleteProvider = vscode.languages.registerCompletionItemProvider(
     ["c", "cpp", "java", "javascript"],
@@ -70,16 +22,10 @@ module.exports = function (parentContext) {
           return undefined;
         }
 
-        const branches = await parentContext.globalState.get("branches");
-        const currentPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-        let organizationName = "";
-        console.log(branches);
-        console.log(currentPath);
-        for (const elem of branches) {
-          if (elem.directory === currentPath) {
-            organizationName = elem.organization;
-          }
-        }
+        const organizationName = await parentContext.workspaceState.get(
+          "organization"
+        );
+        console.log(organizationName);
 
         const response = await axios.get(
           `https://api.github.com/orgs/${organizationName}/members`,
